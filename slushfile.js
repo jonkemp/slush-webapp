39cc990e7a7a5cd4c1720c2a5d2cfa1ef83cf0ee
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-05-13 10:56:01 -0400

    Listen to the 'finish' event rather than process 'exit'.

--- slushfile.js
+++ slushfile.js
@@ -78,32 +78,31 @@ gulp.task('default', function (done) {
                 }))
                 .pipe(conflict('./'))
                 .pipe(gulp.dest('./'))
-                .pipe(install());
+                .pipe(install())
+                .on('finish', function () {
+                    var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
 
-            process.on('exit', function () {
-                var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
-
-                // wire Bower packages to .html
-                wiredep({
-                    bowerJson: bowerJson,
-                    directory: 'app/bower_components',
-                    src: 'app/index.html'
-                });
-
-                if (answers.includeSass) {
-                    // wire Bower packages to .scss
+                    // wire Bower packages to .html
                     wiredep({
                         bowerJson: bowerJson,
                         directory: 'app/bower_components',
-                        src: 'app/styles/*.scss'
+                        src: 'app/index.html'
                     });
-                }
 
-                gutil.log('After running `npm install & bower install`, inject your front end dependencies into');
-                gutil.log('your HTML by running:');
-                gutil.log('  gulp wiredep');
+                    if (answers.includeSass) {
+                        // wire Bower packages to .scss
+                        wiredep({
+                            bowerJson: bowerJson,
+                            directory: 'app/bower_components',
+                            src: 'app/styles*//*.scss'
+                        });
+                    }
 
-                done();
-            });
+                    gutil.log('After running `npm install & bower install`, inject your front end dependencies into');
+                    gutil.log('your HTML by running:');
+                    gutil.log('  gulp wiredep');
+
+                    done();
+                });
         });
 });
