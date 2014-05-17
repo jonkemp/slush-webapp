b78ca50cbb44182164c3105de04898f25e42754c
Jonathan Kemp <kempdogg@gmail.com>
2014-05-16 23:04:40 -0400

    Fix tabs.

--- slushfile.js
+++ slushfile.js
@@ -21,92 +21,87 @@ var gulp = require('gulp'),
 
 gulp.task('default', function (done) {
     inquirer.prompt([
-            {
-                type: 'input',
-                name: 'appname',
-                message: 'Give your app a name',
-                default: gulp.args.join(' ')
+        {
+            type: 'checkbox',
+            name: 'features',
+            message: 'Which other options would you like to include?',
+            choices: [{
+                name: 'Sass',
+                value: 'includeSass',
+                checked: true
             }, {
-                type: 'checkbox',
-                name: 'features',
-                message: 'Which other options would you like to include?',
-                choices: [{
-                    name: 'Sass',
-                    value: 'includeSass',
-                    checked: true
-                }, {
-                    name: 'Bootstrap',
-                    value: 'includeBootstrap',
-                    checked: true
-                }, {
-                    name: 'Modernizr',
-                    value: 'includeModernizr',
-                    checked: true
-                }]
+                name: 'Bootstrap',
+                value: 'includeBootstrap',
+                checked: true
             }, {
-                type: 'confirm',
-                name: 'moveon',
-                message: 'Continue?'
-            }
-        ],
-        function (answers) {
-            var features = answers.features;
+                name: 'Modernizr',
+                value: 'includeModernizr',
+                checked: true
+            }]
+        }, {
+            type: 'confirm',
+            name: 'moveon',
+            message: 'Continue?'
+        }
+    ],
+    function (answers) {
+        var features = answers.features;
 
-            var hasFeature = function (feat) {
-                return features.indexOf(feat) !== -1;
-            };
+        var hasFeature = function (feat) {
+            return features.indexOf(feat) !== -1;
+        };
 
-            answers.includeSass = hasFeature('includeSass');
-            answers.includeBootstrap = hasFeature('includeBootstrap');
-            answers.includeModernizr = hasFeature('includeModernizr');
+        answers.includeSass = hasFeature('includeSass');
+        answers.includeBootstrap = hasFeature('includeBootstrap');
+        answers.includeModernizr = hasFeature('includeModernizr');
 
-            if (!answers.moveon) {
-                return done();
-            }
+        if (!answers.moveon) {
+            return done();
+        }
 
-            answers.appNameSlug = _s.slugify(answers.appname);
+        answers.appNameSlug = _s.slugify(answers.appname);
 
-            gulp.src(__dirname + '/templates/**')
-                .pipe(template(answers))
-                .pipe(rename(function(file) {
-                    if (file.basename[0] === '_') {
-                        file.basename = '.' + file.basename.slice(1);
-                    }
-                    if (answers.includeSass && file.extname === '.css') {
-                        file.extname = '.scss';
-                    }
-                }))
-                .pipe(conflict('./'))
-                .pipe(gulp.dest('./'))
-                .pipe(install());
+        gulp.src(__dirname + '/templates/**')
+            .pipe(template(answers))
+            .pipe(rename(function(file) {
+                if (file.basename[0] === '_') {
+                    file.basename = '.' + file.basename.slice(1);
+                }
+                if (answers.includeSass && file.extname === '.css') {
+                    file.extname = '.scss';
+                }
+            }))
+            .pipe(conflict('./'))
+            .pipe(gulp.dest('./'))
+            .pipe(install());
 
-            process.on('exit', function () {
-                var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
-                var skipInstall = process.argv.slice(2).indexOf('--skip-install') >= 0;
+        process.on('exit', function () {
+            var bowerJson = JSON.parse(fs.readFileSync('./bower.json'));
+            var skipInstall = process.argv.slice(2).indexOf('--skip-install') >= 0;
 
-                if (!skipInstall) {
-                    // wire Bower packages to .html
+            if (!skipInstall) {
+                // wire Bower packages to .html
+                wiredep({
+                    bowerJson: bowerJson,
+                    directory: 'app/bower_components',
+                    src: 'app/index.html'
+                });
+
+                if (answers.includeSass) {
+                    // wire Bower packages to .scss
                     wiredep({
                         bowerJson: bowerJson,
                         directory: 'app/bower_components',
-                        src: 'app/index.html'
+                        src: 'app/styles/*.scss'
                     });
-
-                    if (answers.includeSass) {
-                        // wire Bower packages to .scss
-                        wiredep({
-                            bowerJson: bowerJson,
-                            directory: 'app/bower_components',
-                            src: 'app/styles/*.scss'
-                        });
-                    }
-                } else {
-                    gutil.log('After running `npm install & bower install`, inject your front end dependencies into');
-                    gutil.log('your HTML by running:');
-                    gutil.log('  gulp wiredep');
                 }
+            } else {
+                gutil.log('After running `npm install & bower install`, inject your front end dependencies into');
+                gutil.log('your HTML by running:');
+                gutil.log('  gulp wiredep');
+            }
 
-                done();
-            });
+            done();
         });
+    });
 });
