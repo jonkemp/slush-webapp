bf4207c5937b0219f6fcded24c7c6a3df8552b14
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-06-10 17:37:32 -0400

    Add tests. Bump dependencies. Add devDependencies.

---
+++ test/main.js
@@ -0,0 +1,63 @@
+/* jshint node:true */
+/* global require, it, before, beforeEach, describe */
+
+'use strict';
+var should = require('should'),
+    inquirer = require('inquirer'),
+    gulp = require('gulp'),
+    mockGulpDest = require('mock-gulp-dest')(gulp);
+
+require('../slushfile');
+
+/**
+ * Mock inquirer prompt
+ */
+
+function mockPrompt(answers) {
+    inquirer.prompt = function (prompts, done) {
+
+        [].concat(prompts).forEach(function (prompt) {
+            if (!(prompt.name in answers)) {
+                answers[prompt.name] = prompt.default;
+            }
+        });
+
+        done(answers);
+    };
+}
+
+describe('slush-webapp', function() {
+    before(function () {
+        process.chdir(__dirname);
+        process.argv.push('--skip-install');
+    });
+
+    describe('default generator', function () {
+        beforeEach(function () {
+            mockPrompt({
+                features: [{
+                    name: 'Sass',
+                    value: 'includeSass',
+                    checked: false
+                }, {
+                    name: 'Bootstrap',
+                    value: 'includeBootstrap',
+                    checked: false
+                }, {
+                    name: 'Modernizr',
+                    value: 'includeModernizr',
+                    checked: false
+                }],
+                moveon: true
+            });
+        });
+
+        it('should put all project files in current working directory', function (done) {
+            gulp.start('default').once('stop', function () {
+                mockGulpDest.cwd().should.equal(__dirname);
+                mockGulpDest.basePath().should.equal(__dirname);
+                done();
+            });
+        });
+    });
+});
